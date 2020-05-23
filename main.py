00de9cff7a77f9065ed3f6f7a036b6ff640a74ea
Chase Ries <hello@chaseries.com>
2020-05-23 06:09:51 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,78 @@
+#! /usr/local/bin/python3
+import argparse
+import os
+
+CSS_LINK = "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/main.css\">"
+JS_LINK = "<script src=\"js/main.js\"></script>"
+JS_PLACEHOLDER = "<!-- js -->"
+CSS_PLACEHOLDER = "<!-- css -->"
+
+js_map = { 
+  JS_PLACEHOLDER: JS_LINK,
+}
+
+css_map = {
+  CSS_PLACEHOLDER: CSS_LINK
+}
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "-css",
+    help="Include a default CSS file.",
+    action="store_true")
+
+parser.add_argument(
+    "-js",
+    help="Include a default JavaScript file.",
+    action="store_true")
+
+args = parser.parse_args()
+
+def handle_js():
+  try:
+    os.mkdir(os.path.join(os.getcwd(), "js"))
+  except OSError:
+    print("Creation of JavaScript directory failed.")
+
+def handle_css():
+  try:
+    os.mkdir(os.path.join(os.getcwd(), "css"))
+  except:
+    print("Creation of CSS directory failed.")
+
+def modify_index(js=False, css=False):
+  dirname = os.path.dirname(os.path.realpath(__file__))
+  input_template = os.path.join(dirname, "templates/basic.html")
+  output_template = os.path.join(os.getcwd(), "index.html")
+  with open(input_template, "r") as input_file, open(output_template, "w") as output_file:
+    for line in input_file:
+      stripped_line = line.strip()
+
+      if stripped_line in js_map:
+        if args.js:
+          output_file.write(js_map[stripped_line])
+        else:
+          output_file.write("")
+
+      if stripped_line in css_map:
+        if args.css:
+          output_file.write(css_map[stripped_line])
+        else:
+          output_file.write("")
+
+      if stripped_line not in css_map or js_map:
+        output_file.write(line)
+
+
+if __name__ == '__main__':
+  if args.js:
+    handle_js()
+  if args.css:
+    handle_css()
+  modify_index(args.js, args.css)
+
+
+
+
+
